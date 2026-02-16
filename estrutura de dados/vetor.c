13ff72cdc26f04f78e69c4c091db9e98a1f82b6c
VR-ALTF4 <viniciusroberto.2006@gmail.com>
2026-02-16 15:25:21 -0300

    estudo de vetores

---
+++ estrutura de dados/vetor.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void){
+int vetor[6]= {1,2,3,4,5,6};
+printf("o valor é:%i\n",vetor);
+for(int i = 0;i<6;i++){
+    printf("os valores são %i\n",vetor[i]);
+    
+
+
+
+}
+
+return(0);}  
